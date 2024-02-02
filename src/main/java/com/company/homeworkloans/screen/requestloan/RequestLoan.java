e008bc1deee5970e3669fa9e7e14532acf38f334
Sergei Aksenov <s.aksenov@haulmont.com>
2024-02-02 17:53:29 +0400

    Initial commit

---
+++ src/main/java/com/company/homeworkloans/screen/requestloan/RequestLoan.java
@@ -0,0 +1,10 @@
+package com.company.homeworkloans.screen.requestloan;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("RequestLoan")
+@UiDescriptor("request_loan.xml")
+public class RequestLoan extends Screen {
+}
